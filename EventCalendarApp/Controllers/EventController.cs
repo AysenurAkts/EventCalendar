904e31d966cf01f68a034a5b68ade7d521f4bb25
AysenurAkts <81049064+AysenurAkts@users.noreply.github.com>
2021-10-19 16:13:17 +0300

    Update EventController.cs

--- EventCalendarApp/Controllers/EventController.cs
+++ EventCalendarApp/Controllers/EventController.cs
@@ -18,106 +18,6 @@ namespace EventCalendarApp.Controllers
         {
             _context = context;
         }
-        /*
-        // GET: Event
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Events.ToListAsync());
-        }
-        
-        // GET: Event/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @event = await _context.Events
-                .FirstOrDefaultAsync(m => m.EventId == id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-            return View(@event);
-        }
-
-        // GET: Event/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Event/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EventId,EventTitle,EventStartDate,EventEndDate")] Event @event)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(@event);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@event);
-        }
-
-        // GET: Event/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @event = await _context.Events.FindAsync(id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-            return View(@event);
-        }
-
-        // POST: Event/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EventId,EventTitle,EventStartDate,EventEndDate")] Event @event)
-        {
-            if (id != @event.EventId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(@event);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EventExists(@event.EventId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@event);
-        }
-
-         
-        */
 
         // GET: Event
         public IActionResult Index()
@@ -157,8 +57,6 @@ namespace EventCalendarApp.Controllers
             return new JsonResult(@event);
         }
 
-        
-
         // POST: Event/Edit/5
         [HttpPost]
         public async Task<IActionResult> Edit(int id, [FromBody] System.Text.Json.JsonElement @event)
@@ -192,24 +90,6 @@ namespace EventCalendarApp.Controllers
             }
             return new JsonResult(id);
         }
-
-        // GET: Event/Delete/5
-        /*public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @event = await _context.Events
-                .FirstOrDefaultAsync(m => m.EventId == id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-            return new JsonResult(id);
-        }*/
 
         // POST: Event/Delete/5
         //[HttpPost, ActionName("Delete")]
